f0d3359f09de41a9db977a5743a489adebdb5ffb
Jay Luker <jay_luker@harvard.edu>
2018-03-05 13:56:23 -0500

    removing all the loggly setup stuff :(
    getting to be more levels of hackish than i was comfortable with

--- functions/common.py
+++ functions/common.py
@@ -1,16 +1,15 @@
 
 import logging
 import aws_lambda_logging
+from functools import wraps
 from os import getenv as env
-from pyloggly import LogglyBulkHandler
 
 LOG_LEVEL = env('DEBUG') and 'DEBUG' or 'INFO'
 BOTO_LOG_LEVEL = env('BOTO_DEBUG') and 'DEBUG' or 'INFO'
-LOGGLY_TOKEN = env('LOGGLY_TOKEN')
-LOGGLY_TAGS = env('LOGGLY_TAGS')
 
 def setup_logging(handler_func):
 
+    @wraps(handler_func)
     def wrapped_func(event, context):
 
         extra_info = {'aws_request_id': context.aws_request_id}
@@ -20,29 +19,13 @@ def setup_logging(handler_func):
             **extra_info
         )
 
-        # do this second otherwise aws_lambda_logging clobbers our formatter
-        if LOGGLY_TOKEN:
-            tags = LOGGLY_TAGS and ",".join(LOGGLY_TAGS.split()) or None
-            logger = logging.getLogger()
-            loggly_handler = LogglyBulkHandler(LOGGLY_TOKEN, 'logs-01.loggly.com', tags)
-            loggly_handler.level = logging.getLevelName(LOG_LEVEL)
-            logger.addHandler(loggly_handler)
-            logger.debug("Logging to loggly!")
+        logger = logging.getLogger()
 
-            # set the urllib level to same as boto
-            logging.getLogger('urllib3').setLevel(logging.getLevelName(BOTO_LOG_LEVEL))
-        else:
-            loggly_handler = None
-
-        # now call the handler
         try:
             retval = handler_func(event, context)
         except Exception as e:
             logger.exception("handler failed!")
             raise
-        finally:
-            if loggly_handler:
-                loggly_handler.flush()
 
         return retval
 
