0fe6b1c1033ae24c0cfad614f7af797f96637068
nataliemona <nataliemona@g.harvard.edu>
2020-11-19 14:15:04 -0500

    full test coverage for schedule parsing

--- tests/test_gsheets.py
+++ tests/test_gsheets.py
@@ -3,9 +3,10 @@ from os.path import dirname, join
 site.addsitedir(join(dirname(dirname(__file__)), 'functions'))
 from importlib import import_module
 import gsheets
+import pytest
 
 
-def test_schedule_parsing(mocker):
+def test_schedule_successful_parsing(mocker):
     mock_table_name = "mock_table_name"
     mock_json_to_dynamo = mocker.patch.object(
         gsheets, "schedule_json_to_dynamo"
@@ -39,17 +40,24 @@ def test_schedule_parsing(mocker):
             ],
             "opencast_series_id": "20210161616",
             "zoom_series_id": "555555555"
-        }
-    }
-
-    schedule2_expected = {
-        "0123456789": {
-            "course_code": "BIOS E-18",
+        },
+        "123123123": {
+            "course_code": "MATH E-55",
             "events": [
-                {"day": "T", "time": "20:10", "title": "Section"}
+                {"day": "T", "time": "14:30", "title": "Lecture"},
+                {"day": "R", "time": "14:30", "title": "Lecture"}
             ],
-            "opencast_series_id": "20210112345",
-            "zoom_series_id": "0123456789"
+            "opencast_series_id": "20210155155",
+            "zoom_series_id": "123123123"
+        },
+        "100100100": {
+            "course_code": "STAT S-100",
+            "events": [
+                {"day": "S", "time": "18:30", "title": "Section"},
+                {"day": "U", "time": "18:30", "title": "Section"}
+            ],
+            "opencast_series_id": "20210110010",
+            "zoom_series_id": "100100100"
         }
     }
 
@@ -59,7 +67,84 @@ def test_schedule_parsing(mocker):
         mock_table_name, schedule_data=schedule1_expected
     )
 
-    gsheets.schedule_csv_to_dynamo(mock_table_name, "tests/input/schedule2.csv")
+
+def test_schedule_missing_req_field(mocker):
+    with pytest.raises(Exception, match="Missing required field"):
+        gsheets.schedule_csv_to_dynamo(
+            "mock_table_name", "tests/input/schedule2.csv"
+        )
+
+
+def test_schedule_missing_zoom_link(mocker):
+    mock_table_name = "mock_table_name"
+    mock_json_to_dynamo = mocker.patch.object(
+        gsheets, "schedule_json_to_dynamo"
+    )
+
+    # 2 meetings but 1 does not have a Zoom link so only 1 meeting added
+    schedule3_expected = {
+        "9876543210": {
+            "course_code": "BIOS E-18",
+            "events": [
+                {"day": "M", "time": "19:40", "title": "Lecture"},
+                {"day": "W", "time": "19:40", "title": "Lecture"}
+            ],
+            "opencast_series_id": "20210155555",
+            "zoom_series_id": "9876543210"
+        }
+    }
+
+    gsheets.schedule_csv_to_dynamo(mock_table_name, "tests/input/schedule3.csv")
     mock_json_to_dynamo.assert_called_with(
-        mock_table_name, schedule_data=schedule2_expected
+        "mock_table_name", schedule_data=schedule3_expected
+    )
+
+
+def test_schedule_invalid_zoom_link(mocker):
+    mock_table_name = "mock_table_name"
+    mock_json_to_dynamo = mocker.patch.object(
+        gsheets, "schedule_json_to_dynamo"
+    )
+
+    # 2 meetings but 1 has an invalid Zoom link so only 1 meeting added
+    schedule4_expected = {
+        "9876543210": {
+            "course_code": "BIOS E-18",
+            "events": [
+                {"day": "M", "time": "19:40", "title": "Lecture"},
+                {"day": "W", "time": "19:40", "title": "Lecture"}
+            ],
+            "opencast_series_id": "20210155555",
+            "zoom_series_id": "9876543210"
+        }
+    }
+
+    gsheets.schedule_csv_to_dynamo(mock_table_name, "tests/input/schedule4.csv")
+    mock_json_to_dynamo.assert_called_with(
+        "mock_table_name", schedule_data=schedule4_expected
+    )
+
+
+def test_schedule_missing_oc_series(mocker):
+    mock_table_name = "mock_table_name"
+    mock_json_to_dynamo = mocker.patch.object(
+        gsheets, "schedule_json_to_dynamo"
+    )
+
+    # 2 meetings but 1 has no opencast series id so only 1 meeting added
+    schedule5_expected = {
+        "9876543210": {
+            "course_code": "BIOS E-18",
+            "events": [
+                {"day": "M", "time": "19:40", "title": "Lecture"},
+                {"day": "W", "time": "19:40", "title": "Lecture"}
+            ],
+            "opencast_series_id": "20210155555",
+            "zoom_series_id": "9876543210"
+        }
+    }
+
+    gsheets.schedule_csv_to_dynamo(mock_table_name, "tests/input/schedule5.csv")
+    mock_json_to_dynamo.assert_called_with(
+        "mock_table_name", schedule_data=schedule5_expected
     )
