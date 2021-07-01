2efbbdb0045be3657c71435a9297e1f1dc9aa3a2
Natalie Mona Matthews <natalie.mona@gmail.com>
2021-07-01 16:23:23 -0400

    b/zip 70 schedule day parsing (#132)
    * Handle mixed spaces day input case

--- tests/test_utils_gsheets.py
+++ tests/test_utils_gsheets.py
@@ -40,6 +40,26 @@ def test_schedule_successful_parsing(mocker):
             "opencast_series_id": "20210161616",
             "zoom_series_id": "555555555",
         },
+        "876876876": {
+            "course_code": "ECON E-10A",
+            "events": [
+                {"day": "M", "time": "11:00", "title": "Lecture"},
+                {"day": "T", "time": "11:00", "title": "Lecture"},
+                {"day": "R", "time": "11:00", "title": "Lecture"},
+            ],
+            "opencast_series_id": "20210187678",
+            "zoom_series_id": "876876876",
+        },
+        "200200200": {
+            "course_code": "ECON E-20A",
+            "events": [
+                {"day": "T", "time": "13:00", "title": "Lecture"},
+                {"day": "R", "time": "13:00", "title": "Lecture"},
+                {"day": "F", "time": "13:00", "title": "Lecture"},
+            ],
+            "opencast_series_id": "20210120020",
+            "zoom_series_id": "200200200",
+        },
         "123123123": {
             "course_code": "MATH E-55",
             "events": [
