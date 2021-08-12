f36b55dbcbcb7044de53859b59b20afc908fd982
nataliemona <nataliemona@g.harvard.edu>
2021-08-12 16:04:18 -0400

    update dashboard generation

--- gen_dashboard_template.py
+++ gen_dashboard_template.py
@@ -1,38 +1,49 @@
-import site
-from os.path import join, dirname
 import subprocess
 import json
 import sys
-
-site.addsitedir(join(dirname(dirname(__file__)), "cdk"))
+import os
 from cdk import names
 
-if len(sys.argv) != 3:
-    print("Usage: python gen_dashboard_template.py source_stack dest_stack")
+if len(sys.argv) < 3 or len(sys.argv) > 4:
+    print(
+        "Usage: python gen_dashboard_template.py source_stack dest_stack [source_aws_profile]"
+    )
 
-COPY_FROM_STACK_NAME = sys.argv[1]
-GEN_FOR_STACK_NAME = sys.argv[2]
+INPUT_STACK_NAME = sys.argv[1]
+OUTPUT_STACK_NAME = sys.argv[2]
 
-cmd = f"aws --profile prod cloudwatch get-dashboard --dashboard-name {COPY_FROM_STACK_NAME}"
+profile_arg = ""
+if len(sys.argv) == 4:
+    profile_arg = f"--profile {sys.argv[3]}"
+
+cmd = f"aws {profile_arg} cloudwatch get-dashboard --dashboard-name {INPUT_STACK_NAME}"
 r = subprocess.run(cmd.split(), capture_output=True)
 
+output = r.stdout.decode("ascii")
+if not output:
+    print(
+        f"Dashboard for {INPUT_STACK_NAME} not found. Are you using the correct AWS profile?"
+    )
+    sys.exit(0)
 body = json.loads(json.loads(r.stdout.decode("ascii"))["DashboardBody"])
 
 with open("source_dashboard.json", "w") as f:
     f.write(json.dumps(body))
 
 with open("source_dashboard.json", "r") as infile:
-    with open("output_dashboard.json", "w") as outfile:
+    with open(f"{OUTPUT_STACK_NAME}_dashboard.json", "w") as outfile:
         for line in infile.readlines():
             # fix function names
             for func in names.FUNCTIONS:
                 if func in line:
-                    old_name = f"{COPY_FROM_STACK_NAME}-{func}-function"
-                    new_name = f"{GEN_FOR_STACK_NAME}-{func}"
+                    old_name = f"{INPUT_STACK_NAME}-{func}-function"
+                    new_name = f"{OUTPUT_STACK_NAME}-{func}"
                     new_name = new_name.replace("ingester", "ingest")
                     line = line.replace(old_name, new_name)
                     print(old_name, new_name)
 
             # fix all other resources
-            line = line.replace(COPY_FROM_STACK_NAME, GEN_FOR_STACK_NAME)
+            line = line.replace(INPUT_STACK_NAME, OUTPUT_STACK_NAME)
             outfile.write(line)
+
+os.remove("source_dashboard.json")
